19acffe0cd2c26c0aee2f3f25a1bd929562945d6
gnoir42 <norudenoir@gmail.com>
2026-04-16 12:13:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,16 @@ import tflite_runtime.interpreter as tflite
 
 st.title("FSL ABC Image Classifier")
 
-labels = {}
-for i in range(25):
-    labels[i] = chr(65 + i)  # chr(65) = 'A'
+# Class mapping
+labels = {
+  0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 
+  5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J', 
+  10: 'K', 11: 'L', 12: 'M', 13: 'N', 14: 'O', 
+  15: 'P', 16: 'Q', 17: 'R', 18: 'S', 19: 'T', 
+  20: 'U', 21: 'V', 22: 'W', 23: 'X', 24: 'Y', 
+  25: 'Z'
+}
+
 
 @st.cache_resource
 def load_model():
@@ -52,7 +59,7 @@ if uploaded_file:
         pred_class = int(np.argmax(prediction))
         confidence = float(np.max(prediction))
 
-        # Convert index to letter using dictionary
+        # Convert index to letter
         predicted_letter = labels.get(pred_class, "Unknown")
 
         st.success(f"Prediction: {predicted_letter}")
