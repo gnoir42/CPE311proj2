df3a5a7ea06a48b20ea52ece478b309a483b6f46
gnoir42 <norudenoir@gmail.com>
2026-04-18 17:55:05 +0800

    Updated with the 3 models

--- app.py
+++ app.py
@@ -7,25 +7,44 @@ st.title("FSL ABC Image Classifier")
 
 # Class label mapping
 labels = {
-  0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 
-  5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J', 
-  10: 'K', 11: 'L', 12: 'M', 13: 'N', 14: 'O', 
-  15: 'P', 16: 'Q', 17: 'R', 18: 'S', 19: 'T', 
-  20: 'U', 21: 'V', 22: 'W', 23: 'X', 24: 'Y', 
-  25: 'Z'
+  0:'A',1:'B',2:'C',3:'D',4:'E',5:'F',6:'G',7:'H',8:'I',9:'J',
+  10:'K',11:'L',12:'M',13:'N',14:'O',15:'P',16:'Q',17:'R',18:'S',
+  19:'T',20:'U',21:'V',22:'W',23:'X',24:'Y',25:'Z'
 }
 
+# -----------------------------
+# MODEL SELECTION
+# -----------------------------
+model_choice = st.selectbox(
+    "Choose Model",
+    ("Baseline CNN", "Hypertuned CNN", "MobileNet")
+)
 
+model_files = {
+    "Baseline CNN": "baseline_quantized.onnx",
+    "Hypertuned CNN": "model.onnx",
+    "MobileNet": "mobilenet.onnx"
+}
+
+model_path = model_files[model_choice]
+
+
+# -----------------------------
+# LOAD MODEL
+# -----------------------------
 @st.cache_resource
-def load_model():
-    return ort.InferenceSession("model.onnx")
+def load_model(model_path):
+    return ort.InferenceSession(model_path)
 
-session = load_model()
+session = load_model(model_path)
 
 input_name = session.get_inputs()[0].name
 output_name = session.get_outputs()[0].name
 
 
+# -----------------------------
+# IMAGE PREPROCESSING
+# -----------------------------
 def preprocess(image):
     image = image.resize((64,64))
     image = np.array(image)
@@ -39,6 +58,9 @@ def preprocess(image):
     return image
 
 
+# -----------------------------
+# IMAGE UPLOAD
+# -----------------------------
 uploaded = st.file_uploader("Upload image", type=["jpg","png","jpeg"])
 
 if uploaded:
